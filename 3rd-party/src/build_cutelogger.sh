9cb5ddb30222abb952837394487a2611d5099f08
Andrey Semenov <blizkrig@gmail.com>
2018-03-27 15:25:45 +0300

    add third party

---
+++ 3rd-party/src/build_cutelogger.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+source environment
+
+cd cutelogger
+mkdir -p BUILD_DIR
+qmake "OBJECTS_DIR=BUILD_DIR" "DESTDIR=${DESTDIR}" "DLLDESTDIR=${BINDIR}" -spec ${QMAKESPEC} CONFIG+=release CuteLogger.pro
+make ${MAKEOPTS}
+rm -rf BUILD_DIR
+if [ ! -d ${HEADERDIR} ]; 
+  then mkdir "${HEADERDIR}"; 
+fi
+cp -r include/* "${HEADERDIR}"
+rm Makefile
